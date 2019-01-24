817640ba2f64a3b8cb57e6cc0c62509e98c740b0
cc <chao.pan@atxchina.cn>
2019-01-24 15:03:32 +0800

    add target JBL-C220

---
+++ config/JBL-C220-L/res_audio_data_cn.h
@@ -0,0 +1,176 @@
+#ifndef __RES_AUDIO_DATA_CN_H
+#define __RES_AUDIO_DATA_CN_H
+
+const U8 CN_POWER_ON [] = {
+
+};
+
+const U8 CN_POWER_OFF [] = {
+
+};
+
+const U8 CN_SOUND_ZERO[] = { 
+
+};
+
+const U8 CN_SOUND_ONE[] = { 
+
+};
+
+const U8 CN_SOUND_TWO[] = { 
+
+};
+
+const U8 CN_SOUND_THREE[] = { 
+
+};
+
+const U8 CN_SOUND_FOUR[] = { 
+
+};
+
+const U8 CN_SOUND_FIVE[] = { 
+
+};
+
+const U8 CN_SOUND_SIX[] = { 
+
+};
+
+const U8 CN_SOUND_SEVEN[] = { 
+
+};
+
+const U8 CN_SOUND_EIGHT[] = { 
+
+};
+
+const U8 CN_SOUND_NINE[] = { 
+
+};
+
+const U8 CN_BT_PAIR_ENABLE[] = {
+
+};
+
+const U8 CN_BT_PAIRING [] = {
+
+};
+
+const U8 CN_BT_PAIRING_FAIL[] = {
+
+};
+
+const U8 CN_BT_PAIRING_SUCCESS[] = { 
+
+};
+
+const U8 CN_BT_REFUSE[] = { 
+
+};
+
+const U8 CN_BT_OVER[] = { 
+
+};
+
+const U8 CN_BT_ANSWER[] = { 
+
+};
+
+
+const U8 CN_BT_HUNG_UP[] = { 
+
+};
+
+const U8 CN_BT_INCOMING_CALL [] = {
+
+};
+
+const U8 CN_BT_CONNECTED [] = {
+
+};
+
+const U8 CN_BT_DIS_CONNECT [] = {
+
+};
+
+const U8 CN_CHARGE_PLEASE[] = {
+
+};
+
+const U8 CN_CHARGE_FINISH[] = {
+
+};
+
+const U8 CN_LANGUAGE_SWITCH[] = { 
+
+};
+
+const U8 CN_BT_WARNING[] = { 
+
+};
+const U8 CN_BT_VOL_MIN [] = {
+
+};
+
+const U8 CN_BT_VOL_MAX[] = {
+
+};
+//Modified by ATX : Leon.He_20180109
+const U8 CN_BT_LONG_DOUBLE_LOW[] = {
+
+};
+
+const U8 CN_BT_SHORT_1[] = {
+
+};
+
+//Modified by ATX : Parker.Wei_2018328
+const U8 CN_BT_USER_1[] = {
+
+};
+
+const U8 CN_BT_USER_2[] = {
+
+};
+#ifdef __TWS__
+
+const U8 CN_BT_TWS_ISMASTER[] = {
+
+};
+
+const U8 CN_BT_TWS_ISSLAVE[] = {
+
+};
+
+const U8 CN_BT_TWS_SEARCH[] = {
+
+};
+
+const U8 CN_BT_TWS_STOPSEARCH[] = {
+
+};
+
+const U8 CN_BT_TWS_DISCOVERABLE[] = {
+
+};
+
+const U8 CN_BT_TWS_LEFTCHNL[] = {
+
+};
+
+const U8 CN_BT_TWS_RIGHTCHNL[] = {
+
+};
+
+const U8 CN_BT_TWS_CONNECTED[] = {
+
+};
+
+const U8 CN_BT_TWS_PAIRING[] = {
+
+};
+#endif  
+
+#endif
+
